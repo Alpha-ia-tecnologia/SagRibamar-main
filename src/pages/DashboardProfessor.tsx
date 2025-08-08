45fa0f53b9583a2a8fc3446e6e9fc85740919879
hendricksonweib <hendricksonrocha22@gmail.com>
2025-08-08 10:47:18 -0300

    Feat: dashboard professor

--- src/pages/DashboardProfessor.tsx
+++ src/pages/DashboardProfessor.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Header } from "../components/Header";
+import { PageHeader } from "../ui/PageHeader";
 
 export default function DashboardProfessor() {
   return (
-    <div>DashboardProfessor</div>
+    <>
+     <Header />
+     <div className="pt-20 p-12 bg-gray-100 min-h-screen">
+        <PageHeader
+            title="Dashboard"
+            description="Visão geral do Sistema de Avaliação e Gerenciamento"
+        />
+     </div>
+    </>
   )
 }
