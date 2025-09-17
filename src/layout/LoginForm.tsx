e22b850f9edf00971af6c6245f0a4e3970c643ac
carlos-cj-dev <cmj.acad@gmail.com>
2025-09-17 10:18:43 -0300

    adicionando vite name na tela login 5

--- src/layout/LoginForm.tsx
+++ src/layout/LoginForm.tsx
@@ -25,7 +25,7 @@ export const LoginForm = () => {
       onSubmit={handleSubmit}
       className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm"
     >
-      <h1 className="text-2xl font-bold text-center text-blue-600 mb-1">{appName}</h1>
+      <h1 className="text-2xl font-bold text-center text-blue-600 mb-1">SAG (Ribamar)</h1>
       <p className="text-center text-sm text-gray-600 mb-6">
         Sistema de Avaliação e Gerenciamento
       </p>
