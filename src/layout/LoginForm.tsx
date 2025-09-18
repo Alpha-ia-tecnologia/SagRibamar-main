fdec0651b4548dbd25b7bf4be79a545f3a59cdbb
carlos-cj-dev <cmj.acad@gmail.com>
2025-09-18 10:19:27 -0300

    add log na url api no login

--- src/layout/LoginForm.tsx
+++ src/layout/LoginForm.tsx
@@ -12,6 +12,9 @@ export const LoginForm = () => {
 
   const appName = import.meta.env.VITE_APP_NAME ?? "SAG";
 
+  console.log("🔎 VITE_API_URL:", import.meta.env.VITE_API_URL);
+  console.log("🔎 VITE_APP_NAME:", appName);
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const success = await login(email, senha);
