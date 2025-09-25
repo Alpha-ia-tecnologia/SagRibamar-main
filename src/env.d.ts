8f9a5e3c8abbb3a0dfea51320ee6b486e91e5008
carlos-cj-dev <cmj.acad@gmail.com>
2025-09-25 14:46:11 -0300

    add url corretor

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ declare global {
     __ENV__?: {
       API_URL?: string;
       APP_NAME?: string;
+      CORRETOR_URL?: string;
     };
   }
 }
@@ -12,3 +13,4 @@ export {};
 // uso
 const api = window.__ENV__?.API_URL ?? import.meta.env.VITE_API_URL ?? "https://fallback";
 const appName = window.__ENV__?.APP_NAME ?? "SAG";
+const corretorUrl = window.__ENV__?.CORRETOR_URL ?? import.meta.env.VITE_URL_GABARITOS ?? "https://fallback-gabarito";
